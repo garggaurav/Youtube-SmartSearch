3198c810a4be6c8aeb69ef7154327c905da487eb
garggaurav <garggaurav@berkeley.edu>
2014-10-14 03:04:06 -0700

    Also displays video length and added commas in views

--- video.js
+++ video.js
@@ -7,7 +7,7 @@ var s = document.createElement('script');
 s.type = 'text/javascript';
 var code = 'var orig = document.getElementById("watch7-sidebar-modules").innerHTML; ' +
 'var querystr = "";' +
-'var searchbar = \'<form onsubmit="executeSearch(); return false"> <input type="text" id="queryinput"> <input type="submit" id="side-search-button" value="Search"></form> \' ;' + 
+'var searchbar = \'<form onsubmit="executeSearch(); return false"> <input type="text" id="queryinput"> <input type="submit" id="side-search-button" value="Search"></form> </br> \' ;' + 
 
 'document.getElementById("watch7-sidebar-modules").innerHTML = searchbar + orig;' +
 
@@ -16,7 +16,7 @@ var code = 'var orig = document.getElementById("watch7-sidebar-modules").innerHT
 'querystr = document.getElementById("queryinput").value; ' +
 'if(querystr=="")' +
 '{' +
-   ' document.getElementById("watch7-sidebar-modules").innerHTML = searchbar + orig;' +
+   //'document.getElementById("watch7-sidebar-modules").innerHTML = searchbar + orig;' +
 '}' +
 'else {' +
 'var script2 = document.createElement("script");' +
@@ -24,7 +24,30 @@ var code = 'var orig = document.getElementById("watch7-sidebar-modules").innerHT
 'script2.type = "text/javascript";' +
 'document.getElementsByTagName("head")[0].appendChild(script2);' +
 'console.log(querystr);' +
-'}}' +
+'}} ' +
+
+'function getTime(s)' +
+'{' +
+      "if(s[0] == '0' && s[1] == '0')" +
+            'return s.substring(3, 8);' +
+
+      'return s.substring(0, 8);' +
+'} ' + 
+
+'function getViews(s)' +
+'{' +
+      'var ret = "";' +
+      'var c = 0;' +
+      'for(var i = s.length-1; i >= 0; i--)' +
+      '{' +
+            'if(c!=0 && c%3==0)' +
+                  'ret = "," + ret;' +
+
+            'ret = s[i] + ret;' +
+            'c++;' +
+      '}' +
+      'return ret;'+
+'} ' + 
 
 'function SearchYouTube(data)' +  
             '{' +
@@ -37,9 +60,10 @@ var code = 'var orig = document.getElementById("watch7-sidebar-modules").innerHT
             'row += "<a href="+ data.feed.entry[i].media$group.media$player[0].url +"><img width=\'120px\' height=\'80px\' src=" + data.feed.entry[i].media$group.media$thumbnail[0].url + " /></a>";' +
             'row += "</td>";' +
             'row += "<td vAlign=\'top\' width=\'100%\' align=\'left\'>";' +
-            'row += "<a href="+ data.feed.entry[i].media$group.media$player[0].url + "><b>" + data.feed.entry[i].media$group.media$title.$t + "</b></a><br/>";' +
-            'row += "<span style=\'font-size:12px; color:#555555\'>by " + data.feed.entry[i].author[0].name.$t + "</span><br/>";' + 
-            'row += "<span style=\'font-size:12px\' color:#666666>" + data.feed.entry[i].yt$statistics.viewCount + " views" + "<span><br/>";' +
+            'row += "&nbsp <a href="+ data.feed.entry[i].media$group.media$player[0].url + ">&nbsp<b>" + data.feed.entry[i].media$group.media$title.$t + "</b></a><br/>";' +
+            'row += "&nbsp <span style=\'font-size:12px; color:#555555\'>&nbsp by " + data.feed.entry[i].author[0].name.$t + "</span><br/>";' + 
+            'row += "&nbsp &nbsp <span style=\'font-size:12px\' color:#666666>" + getViews(data.feed.entry[i].yt$statistics.viewCount) + " views" + "<span><br/>";' +
+            'row += "&nbsp &nbsp <span style=\'font-size:12px\' color:#666666>" + getTime(data.feed.entry[i].media$group.media$thumbnail[0].time) + "<span><br/>";' +
             'row += "</td>";' +
             'row += "</tr>";' +
             'row += "</table>";' +
@@ -55,7 +79,6 @@ document.body.appendChild(s);
 s.text = code;
 document.body.appendChild(s);
 }
-
 
 
 
